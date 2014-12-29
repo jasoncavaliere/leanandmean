7e3926c30f09d5b9dea37ba813e52123e27a072c
Jason <jasoncavaliere@hotmail.com>
2014-12-29 17:33:45 -0600

    Working sample for Angular Authentication post

--- Samples/AngularAuthentication/js/factories/loginService.js
+++ Samples/AngularAuthentication/js/factories/loginService.js
@@ -1,30 +1,38 @@
 define(['angularAMD'], function (angularAMD) {
     'use strict';
     angularAMD.service('loginService', ['$http',function ($http) {
-        var self=this;
-        self.instance = {};
-        self.instance.token = '';
-        self.instance.isLoggedIn=function(){
-            self.instance.token!='';
-        };
-        self.instance.isAuthorized = function(){
-            if(!self.instance.isLoggedIn()){
-                return false;
+
+        var service = {
+            token : '',
+            loggedInUserName:'',
+            isLoggedIn:function(){
+                return service.token!='';
+            },
+            isAuthorized : function(){
+                if(!service.isLoggedIn()){
+                    return false;
+                }
+                //todo - add oauth type code here...
+                return true;
             }
-            //todo - add oauth type code here...
-            return true;
-        };
-        self.instance.doLogin=function(username,password){
-            alert('logging in as ' + username + '/' + password);
-            self.instance.token= 'test token';
-        };
-
-
-        return self.instance;
+            ,doLogin:function(username,password,oncomplete){
+                service.token= 'test token';
+                service.loginWatcher=true;
+                service.loggedInUserName=username;
+                if(oncomplete){
+                    oncomplete();
+                }
+            }
+            ,doLogout:function(oncomplete){
+                service.token= '';
+                service.loggedInUserName='';
+                if(oncomplete){
+                    oncomplete();
+                }
+            }
+        }
+        return service ;
     }]);
-
-
-
 });
 
 
